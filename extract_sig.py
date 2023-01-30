00af7652bef35a66823dffe4cf72348a5e845dbc
samir <sc9018@ic.ac.uk>
2023-01-30 10:30:41 +0000

    added textfile function

--- extract_sig.py
+++ extract_sig.py
@@ -4,24 +4,40 @@ from MNIST_funcs import stream_normalise_mean_and_range as snmr
 from MNIST_funcs import stream_normalise_mean_and_std as snms
 import MNIST_funcs
 import esig
+import os, os.path
+import errno
+from pathlib import Path
 
 #Extract sequences
 N_CPU = joblib.cpu_count()
-train_points, train_inputs, train_targets = MNIST_funcs.mnist_train_data("seq_MNIST", N_CPU)
-test_points, test_inputs, test_targets = MNIST_funcs.mnist_test_data("seq_MNIST", N_CPU)
+def extract_sequences():
+    N_CPU = joblib.cpu_count()
+    train_points, train_inputs, train_targets = MNIST_funcs.mnist_train_data("seq_MNIST", N_CPU)
+    test_points, test_inputs, test_targets = MNIST_funcs.mnist_test_data("seq_MNIST", N_CPU)
+    return train_points,train_inputs,train_targets,test_points,test_inputs,test_targets
 
 #Write into textfiles
+def write_to_text(input_list):
+    content = '\n'.join(str(i) for i in input_list)
+    p = Path('./Data/')
+    p.mkdir(exist_ok=True)
+    filename = '{}.txt'.format(input_list)
+    with (p/filename).open('w') as opened_file:
+        opened_file.writelines(content)
+
 
 #Compute signatures
 SIGNATURE_LEVEL = 8
 
-train_sigs_snmr = Parallel(n_jobs=N_CPU)([delayed(esig.stream2sig)(snmr(train_points[k]), SIGNATURE_LEVEL)
-                                          for k in range(len(train_points)) ])    
-test_sigs_snmr = Parallel(n_jobs=N_CPU)([delayed(esig.stream2sig)(snmr(test_points[k]), SIGNATURE_LEVEL)
-                                         for k in range(len(test_points)) ])
-    
-train_sigs_snms = Parallel(n_jobs=N_CPU)([ delayed(esig.stream2sig)(snms(train_points[k]), SIGNATURE_LEVEL)
-                                          for k in range(len(train_points)) ])    
-test_sigs_snms = Parallel(n_jobs=N_CPU)([ delayed(esig.stream2sig)(snms(test_points[k]), SIGNATURE_LEVEL)
-                                         for k in range(len(test_points)) ])
+def sigcomp(train_points,test_points,SIGNATURE_LEVEL):
+    train_sigs_snmr = Parallel(n_jobs=N_CPU)([delayed(esig.stream2sig)(snmr(train_points[k]), SIGNATURE_LEVEL)
+                                            for k in range(len(train_points)) ])    
+    test_sigs_snmr = Parallel(n_jobs=N_CPU)([delayed(esig.stream2sig)(snmr(test_points[k]), SIGNATURE_LEVEL)
+                                            for k in range(len(test_points)) ])
+        
+    train_sigs_snms = Parallel(n_jobs=N_CPU)([ delayed(esig.stream2sig)(snms(train_points[k]), SIGNATURE_LEVEL)
+                                            for k in range(len(train_points)) ])    
+    test_sigs_snms = Parallel(n_jobs=N_CPU)([ delayed(esig.stream2sig)(snms(test_points[k]), SIGNATURE_LEVEL)
+                                            for k in range(len(test_points)) ])
+    return train_sigs_snmr,train_sigs_snms,test_sigs_snmr,test_sigs_snms
 
