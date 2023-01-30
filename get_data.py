ee0acef8c1862e8027a8f5c92c6064cafad4180f
samir <sc9018@ic.ac.uk>
2023-01-30 09:25:09 +0000

    innit

---
+++ get_data.py
@@ -0,0 +1,18 @@
+import sys
+import os.path
+import urllib.request
+import tarfile
+
+
+
+##import sequence MNIST
+host_path = "https://raw.githubusercontent.com/edwin-de-jong/mnist-digits-stroke-sequence-data/master/sequences.tar.gz"
+seq_MNIST_path = "seq_MNIST" #local path to store the data
+
+def get_seq_MNIST(seq_MNIST_host_path = host_path, loc_path = seq_MNIST_path):
+    os.makedirs(seq_MNIST_path,exist_ok=True)
+    tgz_file = "seq_MNIST.tgz"
+    urllib.request.urlretrieve(host_path,tgz_file)
+    seq_MNIST_tgz = tarfile.open(tgz_file)
+    seq_MNIST_tgz.extractall(path=seq_MNIST_path)
+    seq_MNIST_tgz.close() 
