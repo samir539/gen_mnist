bbb2d484f251ca0c1b0cd49b96817478da8b0bf2
samir <sc9018@ic.ac.uk>
2023-01-30 15:06:22 +0000

    get beta vals

--- forward_process.py
+++ forward_process.py
@@ -6,7 +6,7 @@ import matplotlib as plt
 # read in signatures
 # apply noise
 
-def beta_vals(timesteps,start=0.0001,stop=0.02):
+def get_beta(timesteps,start=0.0001,stop=0.02):
     """
     generate linspace
     :param timesteps: the number of steps of noise addition
@@ -14,5 +14,13 @@ def beta_vals(timesteps,start=0.0001,stop=0.02):
     :param stop: ending value of beta
     """
     return torch.linspace(start,stop,timesteps)
+
+#based ib x0 (the inital value of the signature) we wish to get the noisy verson for any arbirary time step t
+#q(x_t | x_0) = N(x_t; sqrt(alpha_hat_t)*x_0,(1-alpha_hat_t)I)
+
+#beta value schedule
+T = 200
+beta_vals = get_beta(200)
+
 
 
