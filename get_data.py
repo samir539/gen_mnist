bf2c6514c9bd1a0da768a6b97690f3b13c935272
samir <sc9018@ic.ac.uk>
2023-01-30 09:36:14 +0000

    comment

--- get_data.py
+++ get_data.py
@@ -9,6 +9,7 @@ seq_MNIST_path = "seq_MNIST" #local path to store the data
 
 def get_seq_MNIST(seq_MNIST_host_path = host_path, loc_path = seq_MNIST_path):
     if os.path.exists(seq_MNIST_path):
+        print("--- data already loaded ---") #this is not necessarily true just means the directory exists
         return None
     else:
         os.makedirs(seq_MNIST_path,exist_ok=True)
